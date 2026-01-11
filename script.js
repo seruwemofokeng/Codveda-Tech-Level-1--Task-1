3f23419bfa22acbc2347e5d60ca4c56f82af043a
Seruwe Kenneth Mofokeng <114590573+seruwemofokeng@users.noreply.github.com>
2026-01-11 23:25:20 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('a').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute('href'))
+      .scrollIntoView({ behavior: 'smooth' });
+  });
+});
